9c205bb2658658effea60a0dbd2f118d2676cec8
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2008-09-26 03:35:02 +0000

    0.07 stuff

--- lib/Carp/POE.pm
+++ lib/Carp/POE.pm
@@ -9,7 +9,7 @@ use base qw(Exporter);
 our @EXPORT      = qw(confess croak carp);
 our @EXPORT_OK   = qw(cluck verbose);
 our @EXPORT_FAIL = qw(verbose);
-our $VERSION     = '0.05';
+our $VERSION     = '0.07';
 
 # from POE::Session
 my ($file, $line) = (CALLER_FILE, CALLER_LINE);
